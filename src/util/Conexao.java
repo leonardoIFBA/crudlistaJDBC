0789ce3d6bd53985e8ea21716d0d7f176dc04f70
leonardoIFBA <162935511+leonardoIFBA@users.noreply.github.com>
2025-04-26 07:14:48 -0300

    Update Conexao.java

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -9,12 +9,12 @@ public class Conexao {
         Connection conexao = null;
         String url = "jdbc:mysql://localhost:3306/listacrud?createDatabaseIfNotExist=true";
         String usuario = "root";
-        String senha = "root";
+        String senha = "";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             //Passei os parametros diretamente para a função
             //conexao = DriverManager.getConnection("jdbc:mysql://10.28.0.35:3306/demojpa", "remoto", "remoto");
-            //conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/listacrud?createDatabaseIfNotExist=true", "root", "root");
+            //conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/listacrud?createDatabaseIfNotExist=true", "root", "");
             conexao = DriverManager.getConnection(url, usuario, senha);
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Não foi possível se conectar ao banco");
